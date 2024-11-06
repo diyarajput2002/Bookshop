f9b93dfb54325c21db86dd2ecac6a6e86132e008
Laxmi Singh <130829715+diyarajput2002@users.noreply.github.com>
2024-11-06 23:35:01 +0530

    Update Footer.jsx with link tag from react-router-dom

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -6,10 +7,8 @@ function Footer() {
       <hr />
       <footer className="footer footer-center p-10 text-base-content rounded dark:bg-slate-900 dark:text-white">
         <nav className="grid grid-flow-col gap-4">
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <Link to='/about'  className="link link-hover">About us</Link>
+          <Link to='/contact' className="link link-hover">Contact</Link>
         </nav>
         <nav>
           <div className="grid grid-flow-col gap-4">
@@ -49,7 +48,7 @@ function Footer() {
           </div>
         </nav>
         <aside>
-          <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p>
+          <p>Copyright © 2024 - All right reserved by Laxmi Singh.</p>
         </aside>
       </footer>
     </div>
